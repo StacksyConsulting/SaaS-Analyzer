a10cb4edaa2b0457a807babf6efcb8273022a80e
StacksyConsulting <bryant@stacksyconsulting.com>
2025-09-04 13:01:27 +1000

    Create server.ts

---
+++ utils/supabase/server.ts
@@ -0,0 +1,14 @@
+// utils/supabase/server.ts
+
+import { createServerClient } from "@supabase/ssr"
+import { cookies } from "next/headers"
+
+export const createClient = () =>
+  createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies,
+    }
+  )
+Add Supabase server utility
